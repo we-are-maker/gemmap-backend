8e409302bc24e9415646c65598963f6f8f777cf1
Lee Seung Jin <lpromotion00@gmail.com>
2025-09-16 23:45:44 +0900

    Feat: Add test API endpoint
    - /api/v1/test GET 엔드포인트 추가
    - "success" 응답을 반환하는 테스트용 컨트롤러 구현
    - 기본 Spring Boot 애플리케이션 구조 설정

---
+++ src/main/java/com/gemmap/gemmap/GemmapApplication.java
@@ -0,0 +1,13 @@
+package com.gemmap.gemmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GemmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GemmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gemmap/gemmap/GemmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gemmap.gemmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GemmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
